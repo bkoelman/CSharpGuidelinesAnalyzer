0c22934cc95ddec76c6e81863abcf8f121bb66fe
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-10-10 00:25:49 +0200

    Added analyzer for AV1710

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Naming/DoNotIncludeContainingTypeNameInMembersAnalyzer.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Naming/DoNotIncludeContainingTypeNameInMembersAnalyzer.cs
@@ -10,22 +10,49 @@ namespace CSharpGuidelinesAnalyzer.Naming
     {
         public const string DiagnosticId = "AV1710";
 
-        private const string Title = "AV1710";
-        private const string MessageFormat = "AV1710";
+        private const string Title = "Do not repeat the name of the containing type in member names.";
+        private const string MessageFormat = "{0} {1} contains the name of its containing type {2}.";
         private const string Description = "Don't repeat the name of a class or enumeration in its members.";
         private const string Category = "Naming";
 
         [NotNull]
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
-            Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+            Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description,
+            helpLinkUri: HelpLinkUris.GetForCategory(Category));
 
         [ItemNotNull]
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
+        private static readonly ImmutableArray<SymbolKind> SymbolKinds =
+            new[] { SymbolKind.Property, SymbolKind.Method, SymbolKind.Field, SymbolKind.Event }.ToImmutableArray();
+
         public override void Initialize([NotNull] AnalysisContext context)
         {
-            //context.EnableConcurrentExecution();
-            //context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSymbolAction(AnalyzeMember, SymbolKinds);
+        }
+
+        private void AnalyzeMember(SymbolAnalysisContext context)
+        {
+            string typeName = context.Symbol.ContainingType.Name;
+
+            var method = context.Symbol as IMethodSymbol;
+            switch (method?.MethodKind)
+            {
+                case MethodKind.PropertyGet:
+                case MethodKind.PropertySet:
+                case MethodKind.EventAdd:
+                case MethodKind.EventRemove:
+                    return;
+            }
+
+            if (context.Symbol.Name.Contains(typeName))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Symbol.Locations[0], context.Symbol.Kind,
+                    context.Symbol.Name, typeName));
+            }
         }
     }
 }
