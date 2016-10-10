6137812ee1233dec3a7c97daf25fe12431c870a9
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-10-10 14:47:26 +0200

    Added analyzer for AV1564.

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Maintainability/AvoidBooleanParametersAnalyzer.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Maintainability/AvoidBooleanParametersAnalyzer.cs
@@ -2,6 +2,10 @@ using System.Collections.Immutable;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace CSharpGuidelinesAnalyzer.Maintainability
 {
@@ -10,8 +14,8 @@ namespace CSharpGuidelinesAnalyzer.Maintainability
     {
         public const string DiagnosticId = "AV1564";
 
-        private const string Title = "AV1564";
-        private const string MessageFormat = "AV1564";
+        private const string Title = "Parameter is of type bool";
+        private const string MessageFormat = "Parameter {0} is of type bool.";
         private const string Description = "Avoid methods that take a bool flag.";
         private const string Category = "Maintainability";
 
@@ -25,8 +29,90 @@ namespace CSharpGuidelinesAnalyzer.Maintainability
 
         public override void Initialize([NotNull] AnalysisContext context)
         {
-            //context.EnableConcurrentExecution();
-            //context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterSyntaxNodeAction(c => AnalyzeParameter(SyntaxToSymbolContext(c)), SyntaxKind.Parameter);
+        }
+
+        private static SymbolAnalysisContext SyntaxToSymbolContext(SyntaxNodeAnalysisContext syntaxContext)
+        {
+            ISymbol symbol = syntaxContext.SemanticModel.GetDeclaredSymbol(syntaxContext.Node);
+            return SyntaxToSymbolContext(syntaxContext, symbol);
+        }
+
+        private static SymbolAnalysisContext SyntaxToSymbolContext(SyntaxNodeAnalysisContext syntaxContext,
+            [NotNull] ISymbol symbol)
+        {
+            Guard.NotNull(symbol, nameof(symbol));
+
+            return new SymbolAnalysisContext(symbol, syntaxContext.SemanticModel.Compilation, syntaxContext.Options,
+                syntaxContext.ReportDiagnostic, x => true, syntaxContext.CancellationToken);
+        }
+
+        private void AnalyzeParameter(SymbolAnalysisContext context)
+        {
+            var parameter = (IParameterSymbol)context.Symbol;
+
+            if (parameter.Type.SpecialType == SpecialType.System_Boolean)
+            {
+                AnalyzeBooleanParameter(parameter, context);
+            }
+        }
+
+        private void AnalyzeBooleanParameter([NotNull] IParameterSymbol parameter, SymbolAnalysisContext context)
+        {
+            ISymbol containingMember = parameter.ContainingSymbol;
+            if (containingMember.IsOverride)
+            {
+                return;
+            }
+
+            if (HidesBaseMember(containingMember, context.CancellationToken))
+            {
+                return;
+            }
+
+            if (IsInterfaceImplementation(parameter))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, parameter.Locations[0], parameter.Name));
+        }
+
+        private bool HidesBaseMember([NotNull] ISymbol member, CancellationToken cancellationToken)
+        {
+            var syntax = member.DeclaringSyntaxReferences[0].GetSyntax(cancellationToken);
+
+            var method = syntax as MethodDeclarationSyntax;
+            var indexer = syntax as IndexerDeclarationSyntax;
+
+            return ContainsNewModifier(method?.Modifiers ?? indexer?.Modifiers);
+        }
+
+        private static bool ContainsNewModifier([CanBeNull] SyntaxTokenList? modifiers)
+        {
+            return modifiers != null && modifiers.Value.Any(m => m.Kind() == SyntaxKind.NewKeyword);
+        }
+
+        private bool IsInterfaceImplementation([NotNull] IParameterSymbol parameter)
+        {
+            ISymbol containingMember = parameter.ContainingSymbol;
+
+            foreach (INamedTypeSymbol iface in parameter.ContainingType.AllInterfaces)
+            {
+                foreach (ISymbol ifaceMember in iface.GetMembers())
+                {
+                    ISymbol implementer = parameter.ContainingType.FindImplementationForInterfaceMember(ifaceMember);
+
+                    if (containingMember.Equals(implementer))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
