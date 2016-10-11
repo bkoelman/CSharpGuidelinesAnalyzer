2c325136a288bdded2f6047d7299f23fce3fbe07
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-10-11 16:10:13 +0200

    Added analyzer for AV1555.

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Maintainability/AvoidUsingNamedArgumentsSpecs.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Maintainability/AvoidUsingNamedArgumentsSpecs.cs
@@ -11,6 +11,69 @@ namespace CSharpGuidelinesAnalyzer.Test.Specs.Maintainability
     {
         protected override string DiagnosticId => AvoidUsingNamedArgumentsAnalyzer.DiagnosticId;
 
+        [Fact]
+        public void When_using_a_named_argument_of_string_type_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M(int i, string s = null)
+                    {
+                    }
+
+                    void N()
+                    {
+                        M(3, s: [|string.Empty|]);
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_using_a_named_argument_of_boolean_type_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M(bool b = false)
+                    {
+                    }
+
+                    void N()
+                    {
+                        M(b: true);
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_using_a_named_argument_of_nullable_boolean_type_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M(bool? b = null)
+                    {
+                    }
+
+                    void N()
+                    {
+                        M(b: true);
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
         protected override DiagnosticAnalyzer CreateAnalyzer()
         {
             return new AvoidUsingNamedArgumentsAnalyzer();
--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Maintainability/AvoidUsingNamedArgumentsAnalyzer.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Maintainability/AvoidUsingNamedArgumentsAnalyzer.cs
@@ -2,6 +2,7 @@ using System.Collections.Immutable;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Semantics;
 
 namespace CSharpGuidelinesAnalyzer.Maintainability
 {
@@ -10,8 +11,8 @@ namespace CSharpGuidelinesAnalyzer.Maintainability
     {
         public const string DiagnosticId = "AV1555";
 
-        private const string Title = "AV1555";
-        private const string MessageFormat = "AV1555";
+        private const string Title = "Avoid using non-(nullable-)boolean named arguments";
+        private const string MessageFormat = "Parameter {0} in the call to {1} is invoked with a named argument.";
         private const string Description = "Avoid using named arguments.";
         private const string Category = "Maintainability";
 
@@ -25,8 +26,31 @@ namespace CSharpGuidelinesAnalyzer.Maintainability
 
         public override void Initialize([NotNull] AnalysisContext context)
         {
-            //context.EnableConcurrentExecution();
-            //context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(startContext =>
+            {
+                if (AnalysisUtilities.SupportsOperations(startContext.Compilation))
+                {
+                    startContext.RegisterOperationAction(AnalyzeArgument, OperationKind.Argument);
+                }
+            });
+        }
+
+        private void AnalyzeArgument(OperationAnalysisContext context)
+        {
+            var argument = (IArgument)context.Operation;
+
+            if (argument.ArgumentKind == ArgumentKind.Named)
+            {
+                if (argument.Parameter.Type.SpecialType != SpecialType.System_Boolean &&
+                    !AnalysisUtilities.IsNullableBoolean(argument.Parameter.Type))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, argument.Syntax.GetLocation(),
+                        argument.Parameter.Name, argument.Parameter.ContainingSymbol.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat)));
+                }
+            }
         }
     }
 }
