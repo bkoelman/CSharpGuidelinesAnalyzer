0eebc0a4a8b4ab7d57be562a825463f76014e806
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-10-11 14:55:15 +0200

    Added analyzer for AV2235.

---
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/AnalysisUtilities.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpGuidelinesAnalyzer
+{
+    public static class AnalysisUtilities
+    {
+        public static bool SupportsOperations([NotNull] Compilation compilation)
+        {
+            IReadOnlyDictionary<string, string> features = compilation.SyntaxTrees.FirstOrDefault()?.Options.Features;
+            return features != null && features.ContainsKey("IOperation") && features["IOperation"] == "true";
+        }
+    }
+}
--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Framework/FavorAsyncAwaitOverTaskContinueWithAnalyzer.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Framework/FavorAsyncAwaitOverTaskContinueWithAnalyzer.cs
@@ -2,6 +2,8 @@ using System.Collections.Immutable;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Semantics;
+using System.Linq;
 
 namespace CSharpGuidelinesAnalyzer.Framework
 {
@@ -10,8 +12,8 @@ namespace CSharpGuidelinesAnalyzer.Framework
     {
         public const string DiagnosticId = "AV2235";
 
-        private const string Title = "AV2235";
-        private const string MessageFormat = "AV2235";
+        private const string Title = "Call to Task.ContinueWith should be replaced with an async method";
+        private const string MessageFormat = "The call to Task.ContinueWith in {0} should be replaced with an async method.";
         private const string Description = "Favor async/await over the Task.";
         private const string Category = "Framework";
 
@@ -25,8 +27,39 @@ namespace CSharpGuidelinesAnalyzer.Framework
 
         public override void Initialize([NotNull] AnalysisContext context)
         {
-            //context.EnableConcurrentExecution();
-            //context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(startContext =>
+            {
+                if (!AnalysisUtilities.SupportsOperations(startContext.Compilation))
+                {
+                    return;
+                }
+                
+                var taskType = startContext.Compilation.GetTypeByMetadataName("System.Threading.Tasks.Task");
+                var continueWithMethodGroup = taskType.GetMembers("ContinueWith");
+
+                if (taskType != null)
+                {
+                    startContext.RegisterOperationAction(c => AnalyzeInvocation(taskType, continueWithMethodGroup, c), OperationKind.InvocationExpression);
+                }
+            });
+        }
+
+        private void AnalyzeInvocation([NotNull] INamedTypeSymbol taskType, [ItemNotNull] ImmutableArray<ISymbol> continueWithMethodGroup, OperationAnalysisContext context)
+        {
+            var invocation = (IInvocationExpression)context.Operation;
+            if (invocation.TargetMethod.ContainingType.Equals(taskType))
+            {
+                var targetMethodConstructed = invocation.TargetMethod.ConstructedFrom;
+
+                if (continueWithMethodGroup.Any(method => method.Equals(targetMethodConstructed)))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, context.Operation.Syntax.GetLocation(), 
+                        context.ContainingSymbol.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat)));
+                }
+            }
         }
     }
 }
