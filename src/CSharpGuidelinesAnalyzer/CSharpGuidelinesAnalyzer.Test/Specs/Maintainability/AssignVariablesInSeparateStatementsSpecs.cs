fb68d6e59ce8c32d27d03bc1c00b39876cbece38
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-10-14 00:48:57 +0200

    Added analyzer for AV1522.

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Maintainability/AssignVariablesInSeparateStatementsSpecs.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Maintainability/AssignVariablesInSeparateStatementsSpecs.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using CSharpGuidelinesAnalyzer.Maintainability;
 using CSharpGuidelinesAnalyzer.Test.TestDataBuilders;
 using Microsoft.CodeAnalysis.CodeFixes;
@@ -10,6 +11,553 @@ namespace CSharpGuidelinesAnalyzer.Test.Specs.Maintainability
     public class AssignVariablesInSeparateStatementsSpecs : CSharpGuidelinesAnalysisTestFixture
     {
         protected override string DiagnosticId => AssignVariablesInSeparateStatementsAnalyzer.DiagnosticId;
+
+        [Fact]
+        public void When_two_variables_are_declared_in_separate_statements_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i;
+                        int j;
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_two_variables_are_declared_in_a_single_statement_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i, j;
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_two_variables_are_assigned_in_separate_statements_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i, j;
+                        i = 5;
+                        j = 8;
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_two_variables_are_declared_and_assigned_in_separate_statements_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i = 5;
+                        int j = 8;
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
+        public void When_two_variables_are_assigned_in_a_single_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i;
+                        int j;
+                        [|i = j = 5;|]
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'i' and 'j' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_two_variables_are_assigned_multiple_times_in_a_single_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i;
+                        int j;
+                        [|i = j = j = 5;|]
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'i' and 'j' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_two_variables_are_declared_and_assigned_in_a_single_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int j;
+                        [|int i = j = 5;|]
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'i' and 'j' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_three_variables_are_assigned_in_a_single_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M()
+                    {
+                        int i;
+                        int j;
+                        int k;
+                        [|i = (true ? (j = 5) : (k = 7));|]
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'i', 'j' and 'k' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|local1 = field1 = Property1 = this[0] = parameter1 = 5;|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_declared_and_assigned_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|int decl1 = local1 = field1 = Property1 = this[0] = parameter1 = 5;|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'decl1', 'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_for_loop_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|for (local1 = 1, field1 = 2, Property1 = 3, this[0] = 4; true; parameter1 = 5)
+                            {
+                            }|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_foreach_loop_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|foreach (var itr in new int[local1 = field1 = Property1 = this[0] = parameter1 = 5])
+                            {
+                            }|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_while_loop_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|while ((local1 = field1 = Property1 = this[0] = parameter1 = 5) > 0)
+                            {
+                            }|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_do_while_loop_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|do
+                            {
+                            }
+                            while ((local1 = field1 = Property1 = this[0] = parameter1 = 5) > 0);|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_an_if_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|if ((local1 = field1 = Property1 = this[0] = parameter1 = 5) > 0)
+                            {
+                            }|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_switch_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        void M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|switch (local1 = field1 = Property1 = this[0] = parameter1 = 4)
+                            {
+                            }|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_return_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        int M(ref int parameter1)
+                        {
+                            int local1;
+
+                            [|return (local1 = field1 = Property1 = this[0] = parameter1 = 4);|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_yield_return_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .Using(typeof (IEnumerable<>).Namespace)
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private int field1;
+
+                        private int Property1 { get; set; }
+
+                        public abstract int this[int index] { get; set; }
+
+                        IEnumerable<int> M()
+                        {
+                            int local1;
+
+                            [|yield return (local1 = field1 = Property1 = this[0] = 4);|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1' and 'C.this[int]' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_lock_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private object field1;
+
+                        private object Property1 { get; set; }
+
+                        public abstract object this[int index] { get; set; }
+
+                        void M(ref object parameter1)
+                        {
+                            object local1;
+
+                            [|lock (local1 = field1 = Property1 = this[0] = parameter1 = new object())
+                            {
+                            }|]
+                        }
+                    }
+                    ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_using_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private IDisposable field1;
+
+                        private IDisposable Property1 { get; set; }
+
+                        public abstract IDisposable this[int index] { get; set; }
+
+                        void M(ref IDisposable parameter1)
+                        {
+                            IDisposable local1;
+
+                            [|using (local1 = field1 = Property1 = this[0] = parameter1 = null)
+                            {
+                            }|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
+
+        [Fact]
+        public void When_multiple_identifiers_are_assigned_in_a_throw_statement_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new ClassSourceCodeBuilder()
+                .InGlobalScope(@"
+                    abstract class C
+                    {
+                        private Exception field1;
+
+                        private Exception Property1 { get; set; }
+
+                        public abstract Exception this[int index] { get; set; }
+
+                        void M(ref Exception parameter1)
+                        {
+                            Exception local1;
+
+                            [|throw local1 = field1 = Property1 = this[0] = parameter1 = new Exception();|]
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "'local1', 'C.field1', 'C.Property1', 'C.this[int]' and 'parameter1' are assigned in a single statement.");
+        }
 
         protected override DiagnosticAnalyzer CreateAnalyzer()
         {
--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Maintainability/AssignVariablesInSeparateStatementsAnalyzer.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Maintainability/AssignVariablesInSeparateStatementsAnalyzer.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Semantics;
 
 namespace CSharpGuidelinesAnalyzer.Maintainability
 {
@@ -10,8 +14,8 @@ namespace CSharpGuidelinesAnalyzer.Maintainability
     {
         public const string DiagnosticId = "AV1522";
 
-        private const string Title = "AV1522";
-        private const string MessageFormat = "AV1522";
+        private const string Title = "Assign each property, field, parameter or variable in a separate statement.";
+        private const string MessageFormat = "{0} are assigned in a single statement.";
         private const string Description = "Assign each variable in a separate statement.";
         private const string Category = "Maintainability";
 
@@ -23,10 +27,122 @@ namespace CSharpGuidelinesAnalyzer.Maintainability
         [ItemNotNull]
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
+        private readonly ImmutableArray<OperationKind> statementKinds =
+            new[]
+            {
+                OperationKind.VariableDeclarationStatement,
+                OperationKind.SwitchStatement,
+                OperationKind.IfStatement,
+                OperationKind.LoopStatement,
+                OperationKind.ThrowStatement,
+                OperationKind.ReturnStatement,
+                OperationKind.LockStatement,
+                OperationKind.UsingStatement,
+                OperationKind.YieldReturnStatement,
+                OperationKind.ExpressionStatement
+            }.ToImmutableArray();
+
         public override void Initialize([NotNull] AnalysisContext context)
         {
-            //context.EnableConcurrentExecution();
-            //context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+            context.RegisterCompilationStartAction(startContext =>
+            {
+                if (AnalysisUtilities.SupportsOperations(startContext.Compilation))
+                {
+                    startContext.RegisterOperationAction(AnalyzeStatement, statementKinds);
+                }
+            });
+        }
+
+        private void AnalyzeStatement(OperationAnalysisContext context)
+        {
+            var statementWalker = new StatementWalker();
+            statementWalker.Visit(context.Operation);
+
+            if (statementWalker.IdentifiersAssigned.Count > 1)
+            {
+                string identifiers = FormatIdentifierList(statementWalker.IdentifiersAssigned.ToList());
+                Location location = context.Operation.Syntax.GetLocation();
+                context.ReportDiagnostic(Diagnostic.Create(Rule, location, identifiers));
+            }
+        }
+
+        [NotNull]
+        private string FormatIdentifierList([NotNull] [ItemNotNull] IList<string> variableNames)
+        {
+            var messageBuilder = new StringBuilder();
+
+            for (int index = 0; index < variableNames.Count - 1; index++)
+            {
+                string variableName = variableNames[index];
+
+                if (messageBuilder.Length > 0)
+                {
+                    messageBuilder.Append(", ");
+                }
+
+                messageBuilder.Append("'" + variableName + "'");
+            }
+
+            messageBuilder.Append(" and '");
+            messageBuilder.Append(variableNames[variableNames.Count - 1] + "'");
+
+            return messageBuilder.ToString();
+        }
+
+        private sealed class StatementWalker : OperationWalker
+        {
+            [NotNull]
+            [ItemNotNull]
+            public ICollection<string> IdentifiersAssigned { get; } = new HashSet<string>();
+
+            public override void VisitVariableDeclaration([NotNull] IVariableDeclaration operation)
+            {
+                if (operation.InitialValue != null)
+                {
+                    IdentifiersAssigned.Add(operation.Variable.Name);
+                }
+
+                base.VisitVariableDeclaration(operation);
+            }
+
+            public override void VisitAssignmentExpression([NotNull] IAssignmentExpression operation)
+            {
+                RegisterAssignment(operation.Target);
+
+                base.VisitAssignmentExpression(operation);
+            }
+
+            private void RegisterAssignment([NotNull] IOperation operation)
+            {
+                var local = operation as ILocalReferenceExpression;
+                if (local != null)
+                {
+                    IdentifiersAssigned.Add(local.Local.Name);
+                }
+
+                var parameter = operation as IParameterReferenceExpression;
+                if (parameter != null)
+                {
+                    IdentifiersAssigned.Add(parameter.Parameter.Name);
+                }
+
+                var field = operation as IFieldReferenceExpression;
+                if (field != null)
+                {
+                    IdentifiersAssigned.Add(
+                        field.Field.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat));
+                }
+
+                var property = operation as IPropertyReferenceExpression;
+                if (property != null)
+                {
+                    IdentifiersAssigned.Add(
+                        property.Property.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat));
+                }
+            }
         }
     }
 }
