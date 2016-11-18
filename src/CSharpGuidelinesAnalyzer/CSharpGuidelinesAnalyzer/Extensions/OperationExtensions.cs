200caca42ddd11022fbd47f60e4a8f8aad0fed0a
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2016-11-18 15:53:56 +0100

    Refactored TryGetIdentifierInfo into shorter methods with less parameters

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Extensions/OperationExtensions.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Extensions/OperationExtensions.cs
@@ -10,47 +10,75 @@ namespace CSharpGuidelinesAnalyzer.Extensions
         [CanBeNull]
         public static IdentifierInfo TryGetIdentifierInfo([CanBeNull] this IOperation identifier)
         {
-            var local = identifier as ILocalReferenceExpression;
-            if (local != null)
+            var visitor = new IdentifierVisitor();
+            return visitor.Visit(identifier, null);
+        }
+
+        private sealed class IdentifierVisitor : OperationVisitor<object, IdentifierInfo>
+        {
+            [NotNull]
+            public override IdentifierInfo VisitLocalReferenceExpression([NotNull] ILocalReferenceExpression operation,
+                [CanBeNull] object argument)
             {
-                return new IdentifierInfo(local.Local.Name,
-                    local.Local.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat), local.Local.Type,
-                    "Variable");
+                return new IdentifierInfo(operation.Local.Name,
+                    operation.Local.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat),
+                    operation.Local.Type, "Variable");
             }
 
-            var parameter = identifier as IParameterReferenceExpression;
-            if (parameter != null)
+            [NotNull]
+            public override IdentifierInfo VisitParameterReferenceExpression(
+                [NotNull] IParameterReferenceExpression operation, [CanBeNull] object argument)
             {
-                return new IdentifierInfo(parameter.Parameter.Name,
+                return new IdentifierInfo(operation.Parameter.Name,
                     /* CSharpShortErrorMessageFormat returns 'ref int', ie. without parameter name */
-                    parameter.Parameter.Name, parameter.Parameter.Type, parameter.Parameter.Kind.ToString());
+                    operation.Parameter.Name, operation.Parameter.Type, operation.Parameter.Kind.ToString());
             }
 
-            var field = identifier as IFieldReferenceExpression;
-            if (field != null)
+            [NotNull]
+            public override IdentifierInfo VisitFieldReferenceExpression([NotNull] IFieldReferenceExpression operation,
+                [CanBeNull] object argument)
             {
-                return new IdentifierInfo(field.Field.Name,
-                    field.Field.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat), field.Field.Type,
-                    field.Field.Kind.ToString());
+                return CreateForMemberReferenceExpression(operation, operation.Field.Type);
             }
 
-            var property = identifier as IPropertyReferenceExpression;
-            if (property != null)
+            [NotNull]
+            public override IdentifierInfo VisitEventReferenceExpression([NotNull] IEventReferenceExpression operation,
+                [CanBeNull] object argument)
             {
-                return new IdentifierInfo(property.Property.Name,
-                    property.Property.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat),
-                    property.Property.Type, property.Property.Kind.ToString());
+                return CreateForMemberReferenceExpression(operation, operation.Event.Type);
             }
 
-            var method = identifier as IInvocationExpression;
-            if (method != null)
+            [NotNull]
+            public override IdentifierInfo VisitPropertyReferenceExpression(
+                [NotNull] IPropertyReferenceExpression operation, [CanBeNull] object argument)
             {
-                return new IdentifierInfo(method.TargetMethod.Name,
-                    method.TargetMethod.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat),
-                    method.TargetMethod.ReturnType, method.TargetMethod.Kind.ToString());
+                return CreateForMemberReferenceExpression(operation, operation.Property.Type);
             }
 
-            return null;
+            [NotNull]
+            public override IdentifierInfo VisitIndexedPropertyReferenceExpression(
+                [NotNull] IIndexedPropertyReferenceExpression operation, [CanBeNull] object argument)
+            {
+                return CreateForMemberReferenceExpression(operation, operation.Property.Type);
+            }
+
+            [NotNull]
+            private IdentifierInfo CreateForMemberReferenceExpression([NotNull] IMemberReferenceExpression operation,
+                [NotNull] ITypeSymbol memberType)
+            {
+                return new IdentifierInfo(operation.Member.Name,
+                    operation.Member.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat), memberType,
+                    operation.Member.Kind.ToString());
+            }
+
+            [NotNull]
+            public override IdentifierInfo VisitInvocationExpression([NotNull] IInvocationExpression operation,
+                [CanBeNull] object argument)
+            {
+                return new IdentifierInfo(operation.TargetMethod.Name,
+                    operation.TargetMethod.ToDisplayString(SymbolDisplayFormat.CSharpShortErrorMessageFormat),
+                    operation.TargetMethod.ReturnType, operation.TargetMethod.Kind.ToString());
+            }
         }
     }
 }
