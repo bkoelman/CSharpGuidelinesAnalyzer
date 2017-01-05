3180358c65bffd96cd6403c000335c71094664b4
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-01-05 23:49:45 +0100

    Cleanup output from code coverage analysis

--- src/clean.bat
+++ src/clean.bat
@@ -4,5 +4,6 @@ for /R %%x in (obj) do if exist "%%x" rmdir /s /q "%%x" 1>NUL 2>NUL
 
 rmdir /s /q .vs 1>NUL 2>NUL
 rmdir /s /q packages 1>NUL 2>NUL
+rmdir /s /q coverage 1>NUL 2>NUL
 
 del /f /s /q *.user 1>NUL 2>NUL
