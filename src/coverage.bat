8cbf78ef89b974ac22ff1bc035bae8e2ca641813
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-03-13 23:14:10 +0100

    Package updates

--- src/coverage.bat
+++ src/coverage.bat
@@ -1,5 +1,5 @@
 @echo off
 rd /s /q coverage 2>nul
 md coverage
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:".\packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe" -targetargs:".\CSharpGuidelinesAnalyzer\CSharpGuidelinesAnalyzer.Test\bin\Debug\CSharpGuidelinesAnalyzer.Test.dll -noshadow" -filter:"+[CSharpGuidelinesAnalyzer]*  -[CSharpGuidelinesAnalyzer]CSharpGuidelinesAnalyzer.Properties.*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
-.\packages\ReportGenerator.2.5.2\tools\ReportGenerator.exe -targetdir:.\coverage -reports:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:".\packages\xunit.runner.console.2.2.0\tools\xunit.console.x86.exe" -targetargs:".\CSharpGuidelinesAnalyzer\CSharpGuidelinesAnalyzer.Test\bin\Debug\CSharpGuidelinesAnalyzer.Test.dll -noshadow" -filter:"+[CSharpGuidelinesAnalyzer]*  -[CSharpGuidelinesAnalyzer]CSharpGuidelinesAnalyzer.Properties.*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
+.\packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe -targetdir:.\coverage -reports:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
