77dd3b080e4e36adae36e902844d7a80818b019e
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-08-18 22:45:48 +0200

    Fixed broken code coverage (local)

--- src/coverage.bat
+++ src/coverage.bat
@@ -1,5 +1,5 @@
 @echo off
 rd /s /q coverage 2>nul
 md coverage
-"%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" -register:user -target:"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe" -targetargs:".\CSharpGuidelinesAnalyzer\CSharpGuidelinesAnalyzer.Test\bin\Debug\net46\CSharpGuidelinesAnalyzer.Test.dll -noshadow" -filter:"+[CSharpGuidelinesAnalyzer]*  -[CSharpGuidelinesAnalyzer]CSharpGuidelinesAnalyzer.Properties.*" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
-"%USERPROFILE%\.nuget\packages\ReportGenerator.2.5.10\tools\ReportGenerator.exe" -targetdir:.\coverage -reports:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
+"%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe" -register:user -target:"%USERPROFILE%\.nuget\packages\xunit.runner.console\2.2.0\tools\xunit.console.x86.exe" -targetargs:".\CSharpGuidelinesAnalyzer\CSharpGuidelinesAnalyzer.Test\bin\Debug\net46\CSharpGuidelinesAnalyzer.Test.dll -noshadow" -excludebyattribute:*.ExcludeFromCodeCoverage* -hideskipped:All -output:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
+"%USERPROFILE%\.nuget\packages\ReportGenerator\2.5.10\tools\ReportGenerator.exe" -targetdir:.\coverage -reports:.\coverage\CSharpGuidelinesAnalyzerCoverage.xml
