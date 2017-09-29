d54858b23983854ac77af56dd0420b6211ad17f0
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2017-09-29 09:46:57 +0200

    Code coverage updates
    - Package update to ReportGenerator v3.0.0
    - Added filter to exclude unittest classes in the output
    - Moved runner (and cleaner) to root of repo to match closer to AppVeyor settings

--- clean.bat
+++ clean.bat
@@ -2,8 +2,8 @@
 for /R .. %%x in (bin) do if exist "%%x" rmdir /s /q "%%x" 1>NUL 2>NUL
 for /R .. %%x in (obj) do if exist "%%x" rmdir /s /q "%%x" 1>NUL 2>NUL
 
-rmdir /s /q .vs 1>NUL 2>NUL
-rmdir /s /q packages 1>NUL 2>NUL
+rmdir /s /q src\.vs 1>NUL 2>NUL
+rmdir /s /q src\packages 1>NUL 2>NUL
 rmdir /s /q coverage 1>NUL 2>NUL
 
 del /f /s /q *.user 1>NUL 2>NUL
