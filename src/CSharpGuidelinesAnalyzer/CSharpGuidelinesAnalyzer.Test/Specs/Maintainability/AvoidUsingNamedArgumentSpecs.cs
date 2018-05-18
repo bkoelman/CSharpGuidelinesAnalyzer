c66b64df96068a985ff5c5fb7c33cf033c265e64
bkoelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-05-18 17:45:28 +0200

    AV1555: Added test for non-trailing named argument

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Maintainability/AvoidUsingNamedArgumentSpecs.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Maintainability/AvoidUsingNamedArgumentSpecs.cs
@@ -32,6 +32,28 @@ namespace CSharpGuidelinesAnalyzer.Test.Specs.Maintainability
         }
 
         [Fact]
+        internal void When_using_a_non_trailing_named_argument_of_string_type_it_must_be_reported()
+        {
+            // Arrange
+            ParsedSourceCode source = new MemberSourceCodeBuilder()
+                .InDefaultClass(@"
+                    void M(int i, string s = null, int j = 1)
+                    {
+                    }
+
+                    void N()
+                    {
+                        M(3, [|s: string.Empty|], 6);
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source,
+                "Parameter 's' in the call to 'Test.M(int, string, int)' is invoked with a named argument.");
+        }
+
+        [Fact]
         internal void When_using_a_named_argument_of_boolean_type_it_must_be_skipped()
         {
             // Arrange
