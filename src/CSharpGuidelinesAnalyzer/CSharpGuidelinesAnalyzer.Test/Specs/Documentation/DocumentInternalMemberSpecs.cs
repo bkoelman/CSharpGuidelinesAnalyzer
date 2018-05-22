b5a7d27af80de566b6a62eb52cb8b62d99334141
bkoelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-05-22 20:35:52 +0200

    AV2305: Added test for local function

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Documentation/DocumentInternalMemberSpecs.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer.Test/Specs/Documentation/DocumentInternalMemberSpecs.cs
@@ -935,6 +935,33 @@ namespace CSharpGuidelinesAnalyzer.Test.Specs.Documentation
         }
 
         [Fact]
+        internal void When_parameter_in_local_function_is_undocumented_it_must_be_skipped()
+        {
+            // Arrange
+            ParsedSourceCode source = new TypeSourceCodeBuilder()
+                .WithDocumentationComments()
+                .InGlobalScope(@"
+                    namespace N.M
+                    {
+                        public class C
+                        {
+                            /// <summary />
+                            internal void M()
+                            {
+                                void N(int i)
+                                {
+                                }
+                            }
+                        }
+                    }
+                ")
+                .Build();
+
+            // Act and assert
+            VerifyGuidelineDiagnostic(source);
+        }
+
+        [Fact]
         internal void When_internal_method_has_documentation_for_missing_parameter_it_must_be_reported()
         {
             // Arrange
