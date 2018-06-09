b2350e6af65fe4e851355181507bad806d352f48
bkoelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-06-09 21:33:29 +0200

    AV1568: Optimized running time

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Rules/Maintainability/DoNotAssignToParameterAnalyzer.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Rules/Maintainability/DoNotAssignToParameterAnalyzer.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Threading;
 using CSharpGuidelinesAnalyzer.Extensions;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
 
@@ -40,110 +40,26 @@ namespace CSharpGuidelinesAnalyzer.Rules.Maintainability
             context.EnableConcurrentExecution();
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 
-            context.RegisterSyntaxNodeAction(c => c.SkipEmptyName(AnalyzeParameter), SyntaxKind.Parameter);
+            context.RegisterSymbolAction(c => c.SkipEmptyName(AnalyzeMethod), SymbolKind.Method);
             context.RegisterSymbolAction(c => c.SkipEmptyName(AnalyzeProperty), SymbolKind.Property);
             context.RegisterSymbolAction(c => c.SkipEmptyName(AnalyzeEvent), SymbolKind.Event);
+
+            context.RegisterOperationAction(c => c.SkipInvalid(AnalyzeLocalFunction), OperationKind.LocalFunction);
+            context.RegisterOperationAction(c => c.SkipInvalid(AnalyzeAnonymousFunction), OperationKind.AnonymousFunction);
         }
 
-        private void AnalyzeParameter(SymbolAnalysisContext context)
+        private void AnalyzeMethod(SymbolAnalysisContext context)
         {
-            var parameter = (IParameterSymbol)context.Symbol;
+            var method = (IMethodSymbol)context.Symbol;
 
-            if (!(parameter.ContainingSymbol is IMethodSymbol method) || method.IsAbstract)
+            if (ShouldSkip(method) || method.IsPropertyOrEventAccessor())
             {
                 return;
             }
 
             using (var collector = new DiagnosticCollector(context.ReportDiagnostic))
             {
-                AnalyzeParameterUsage(parameter, method, collector, context);
-
-                FilterDuplicateLocations(collector.Diagnostics);
-            }
-        }
-
-        private void AnalyzeParameterUsage([NotNull] IParameterSymbol parameter, [NotNull] IMethodSymbol method,
-            [NotNull] DiagnosticCollector collector, SymbolAnalysisContext context)
-        {
-            if (parameter.RefKind != RefKind.None || parameter.IsSynthesized())
-            {
-                return;
-            }
-
-            if (IsUserDefinedStruct(parameter))
-            {
-                // A user-defined struct can reassign its 'this' parameter on invocation. That's why the compiler dataflow
-                // analysis reports all access as writes. Because that's not very practical, we run our own assignment analysis.
-
-                AnalyzeParameterUsageInMethodSlow(parameter, method, collector, context);
-            }
-            else
-            {
-                AnalyzeParameterUsageInMethod(parameter, method, collector, context);
-            }
-        }
-
-        private bool IsUserDefinedStruct([NotNull] IParameterSymbol parameter)
-        {
-            return parameter.Type.TypeKind == TypeKind.Struct && !IsSimpleType(parameter.Type);
-        }
-
-        private bool IsSimpleType([NotNull] ITypeSymbol type)
-        {
-            return type.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T || SimpleTypes.Contains(type.SpecialType);
-        }
-
-        private static void AnalyzeParameterUsageInMethod([NotNull] IParameterSymbol parameter, [NotNull] IMethodSymbol method,
-            [NotNull] DiagnosticCollector collector, SymbolAnalysisContext context)
-        {
-            SyntaxNode body = method.TryGetBodySyntaxForMethod(context.CancellationToken);
-            if (body != null)
-            {
-                SemanticModel model = context.Compilation.GetSemanticModel(body.SyntaxTree);
-                DataFlowAnalysis dataFlowAnalysis = model.AnalyzeDataFlow(body);
-                if (dataFlowAnalysis.Succeeded)
-                {
-                    if (dataFlowAnalysis.WrittenInside.Contains(parameter))
-                    {
-                        collector.Add(Diagnostic.Create(Rule, parameter.Locations[0], parameter.Name));
-                    }
-                }
-            }
-        }
-
-        private void AnalyzeParameterUsageInMethodSlow([NotNull] IParameterSymbol parameter, [NotNull] IMethodSymbol method,
-            [NotNull] DiagnosticCollector collector, SymbolAnalysisContext context)
-        {
-            IOperation body = method.TryGetOperationBlockForMethod(context.Compilation, context.CancellationToken);
-            if (body != null)
-            {
-                var walker = new AssignmentWalker(parameter);
-                walker.Visit(body);
-
-                if (walker.SeenAssignment)
-                {
-                    collector.Add(Diagnostic.Create(Rule, parameter.Locations[0], parameter.Name));
-                }
-            }
-        }
-
-        private void FilterDuplicateLocations([NotNull] [ItemNotNull] IList<Diagnostic> diagnostics)
-        {
-            for (int index = 0; index < diagnostics.Count; index++)
-            {
-                Diagnostic diagnostic = diagnostics[index];
-
-                Diagnostic[] duplicates = diagnostics
-                    .Where(d => !ReferenceEquals(d, diagnostic) && d.Location == diagnostic.Location).ToArray();
-                if (duplicates.Any())
-                {
-                    foreach (Diagnostic duplicate in duplicates)
-                    {
-                        diagnostics.Remove(duplicate);
-                    }
-
-                    index = 0;
-                }
+                InnerAnalyzeMethod(method, collector, context.Compilation, context.CancellationToken);
             }
         }
 
@@ -176,57 +92,193 @@ namespace CSharpGuidelinesAnalyzer.Rules.Maintainability
         private void AnalyzeAccessorMethod([CanBeNull] IMethodSymbol accessorMethod, [NotNull] DiagnosticCollector collector,
             SymbolAnalysisContext context)
         {
-            if (accessorMethod != null && !accessorMethod.IsAbstract)
+            if (accessorMethod == null || ShouldSkip(accessorMethod))
             {
-                foreach (IParameterSymbol parameter in accessorMethod.Parameters)
+                return;
+            }
+
+            InnerAnalyzeMethod(accessorMethod, collector, context.Compilation, context.CancellationToken);
+        }
+
+        private void FilterDuplicateLocations([NotNull] [ItemNotNull] IList<Diagnostic> diagnostics)
+        {
+            for (int index = 0; index < diagnostics.Count; index++)
+            {
+                Diagnostic diagnostic = diagnostics[index];
+
+                Diagnostic[] duplicates = diagnostics
+                    .Where(d => !ReferenceEquals(d, diagnostic) && d.Location == diagnostic.Location).ToArray();
+                if (duplicates.Any())
                 {
-                    AnalyzeParameterUsage(parameter, accessorMethod, collector, context);
+                    foreach (Diagnostic duplicate in duplicates)
+                    {
+                        diagnostics.Remove(duplicate);
+                    }
+
+                    index = 0;
                 }
+            }
+        }
+
+        private void AnalyzeLocalFunction(OperationAnalysisContext context)
+        {
+            var localFunction = (ILocalFunctionOperation)context.Operation;
+
+            if (ShouldSkip(localFunction.Symbol))
+            {
+                return;
+            }
+
+            using (var collector = new DiagnosticCollector(context.ReportDiagnostic))
+            {
+                InnerAnalyzeMethod(localFunction.Symbol, collector, context.Compilation, context.CancellationToken);
+            }
+        }
+
+        private void AnalyzeAnonymousFunction(OperationAnalysisContext context)
+        {
+            var anonymousFunction = (IAnonymousFunctionOperation)context.Operation;
+
+            if (ShouldSkip(anonymousFunction.Symbol))
+            {
+                return;
+            }
+
+            using (var collector = new DiagnosticCollector(context.ReportDiagnostic))
+            {
+                InnerAnalyzeMethod(anonymousFunction.Symbol, collector, context.Compilation, context.CancellationToken);
+            }
+        }
+
+        private static bool ShouldSkip([NotNull] IMethodSymbol method)
+        {
+            return method.IsAbstract || method.IsSynthesized() || !method.Parameters.Any();
+        }
+
+        private void InnerAnalyzeMethod([NotNull] IMethodSymbol method, [NotNull] DiagnosticCollector collector,
+            [NotNull] Compilation compilation, CancellationToken cancellationToken)
+        {
+            SyntaxNode bodySyntax = method.TryGetBodySyntaxForMethod(cancellationToken);
+            if (bodySyntax == null)
+            {
+                return;
+            }
+
+            IGrouping<bool, IParameterSymbol>[] parameterGrouping = method.Parameters
+                .Where(p => p.RefKind == RefKind.None && !p.IsSynthesized()).GroupBy(IsUserDefinedStruct).ToArray();
+
+            IParameterSymbol[] ordinaryParameters = parameterGrouping.Where(x => !x.Key).SelectMany(x => x).ToArray();
+            if (ordinaryParameters.Any())
+            {
+                AnalyzeOrdinaryParameters(ordinaryParameters, bodySyntax, collector, compilation);
+            }
+
+            IParameterSymbol[] structParameters = parameterGrouping.Where(x => x.Key).SelectMany(x => x).ToArray();
+            if (structParameters.Any())
+            {
+                AnalyzeStructParameters(structParameters, bodySyntax, collector, compilation, cancellationToken);
+            }
+        }
+
+        private bool IsUserDefinedStruct([NotNull] IParameterSymbol parameter)
+        {
+            return parameter.Type.TypeKind == TypeKind.Struct && !IsSimpleType(parameter.Type);
+        }
+
+        private bool IsSimpleType([NotNull] ITypeSymbol type)
+        {
+            return type.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T || SimpleTypes.Contains(type.SpecialType);
+        }
+
+        private void AnalyzeOrdinaryParameters([NotNull] [ItemNotNull] ICollection<IParameterSymbol> parameters,
+            [NotNull] SyntaxNode bodySyntax, [NotNull] DiagnosticCollector collector, [NotNull] Compilation compilation)
+        {
+            DataFlowAnalysis dataFlowAnalysis = TryAnalyzeDataFlow(bodySyntax, compilation);
+            if (dataFlowAnalysis == null)
+            {
+                return;
+            }
+
+            foreach (IParameterSymbol parameter in parameters)
+            {
+                if (dataFlowAnalysis.WrittenInside.Contains(parameter))
+                {
+                    collector.Add(Diagnostic.Create(Rule, parameter.Locations[0], parameter.Name));
+                }
+            }
+        }
+
+        [CanBeNull]
+        private DataFlowAnalysis TryAnalyzeDataFlow([NotNull] SyntaxNode bodySyntax, [NotNull] Compilation compilation)
+        {
+            SemanticModel model = compilation.GetSemanticModel(bodySyntax.SyntaxTree);
+            DataFlowAnalysis dataFlowAnalysis = model.AnalyzeDataFlow(bodySyntax);
+
+            return dataFlowAnalysis.Succeeded ? dataFlowAnalysis : null;
+        }
+
+        private void AnalyzeStructParameters([NotNull] [ItemNotNull] ICollection<IParameterSymbol> parameters,
+            [NotNull] SyntaxNode bodySyntax, [NotNull] DiagnosticCollector collector, [NotNull] Compilation compilation,
+            CancellationToken cancellationToken)
+        {
+            // A user-defined struct can reassign its 'this' parameter on invocation. That's why the compiler dataflow
+            // analysis reports all access as writes. Because that's not very practical, we run our own assignment analysis.
+
+            SemanticModel model = compilation.GetSemanticModel(bodySyntax.SyntaxTree);
+            IOperation bodyOperation = model.GetOperation(bodySyntax);
+
+            if (bodyOperation == null || bodyOperation.HasErrors(compilation, cancellationToken))
+            {
+                return;
+            }
+
+            var walker = new AssignmentWalker(parameters);
+            walker.Visit(bodyOperation);
+
+            foreach (IParameterSymbol parameter in walker.ParametersAssigned)
+            {
+                collector.Add(Diagnostic.Create(Rule, parameter.Locations[0], parameter.Name));
             }
         }
 
         private sealed class AssignmentWalker : OperationWalker
         {
             [NotNull]
-            private readonly IParameterSymbol parameter;
+            private readonly IDictionary<IParameterSymbol, bool> seenAssignmentPerParameter =
+                new Dictionary<IParameterSymbol, bool>();
 
-            public bool SeenAssignment { get; private set; }
+            [NotNull]
+            [ItemNotNull]
+            public ICollection<IParameterSymbol> ParametersAssigned =>
+                seenAssignmentPerParameter.Where(pair => pair.Value).Select(pair => pair.Key).ToArray();
 
-            public AssignmentWalker([NotNull] IParameterSymbol parameter)
+            public AssignmentWalker([NotNull] [ItemNotNull] ICollection<IParameterSymbol> parameters)
             {
-                Guard.NotNull(parameter, nameof(parameter));
-                this.parameter = parameter;
+                Guard.NotNull(parameters, nameof(parameters));
+
+                foreach (IParameterSymbol parameter in parameters)
+                {
+                    seenAssignmentPerParameter[parameter] = false;
+                }
             }
 
             public override void VisitSimpleAssignment([NotNull] ISimpleAssignmentOperation operation)
             {
-                if (IsReferenceToCurrentParameter(operation.Target))
-                {
-                    SeenAssignment = true;
-                    return;
-                }
+                RegisterAssignmentToParameter(operation.Target);
 
                 base.VisitSimpleAssignment(operation);
             }
 
             public override void VisitCompoundAssignment([NotNull] ICompoundAssignmentOperation operation)
             {
-                if (IsReferenceToCurrentParameter(operation.Target))
-                {
-                    SeenAssignment = true;
-                    return;
-                }
+                RegisterAssignmentToParameter(operation.Target);
 
                 base.VisitCompoundAssignment(operation);
             }
 
             public override void VisitIncrementOrDecrement([NotNull] IIncrementOrDecrementOperation operation)
             {
-                if (IsReferenceToCurrentParameter(operation.Target))
-                {
-                    SeenAssignment = true;
-                    return;
-                }
+                RegisterAssignmentToParameter(operation.Target);
 
                 base.VisitIncrementOrDecrement(operation);
             }
@@ -237,11 +289,7 @@ namespace CSharpGuidelinesAnalyzer.Rules.Maintainability
                 {
                     foreach (IOperation element in tuple.Elements)
                     {
-                        if (IsReferenceToCurrentParameter(element))
-                        {
-                            SeenAssignment = true;
-                            return;
-                        }
+                        RegisterAssignmentToParameter(element);
                     }
                 }
 
@@ -250,22 +298,25 @@ namespace CSharpGuidelinesAnalyzer.Rules.Maintainability
 
             public override void VisitArgument([NotNull] IArgumentOperation operation)
             {
-                if (IsReferenceToCurrentParameter(operation.Value))
+                if (operation.Parameter.RefKind == RefKind.Ref || operation.Parameter.RefKind == RefKind.Out)
                 {
-                    if (operation.Parameter.RefKind == RefKind.Ref || operation.Parameter.RefKind == RefKind.Out)
-                    {
-                        SeenAssignment = true;
-                        return;
-                    }
+                    RegisterAssignmentToParameter(operation.Value);
                 }
 
                 base.VisitArgument(operation);
             }
 
-            private bool IsReferenceToCurrentParameter([NotNull] IOperation operation)
+            private void RegisterAssignmentToParameter([NotNull] IOperation operation)
             {
-                return operation is IParameterReferenceOperation parameterReference &&
-                    parameter.Equals(parameterReference.Parameter);
+                if (operation is IParameterReferenceOperation parameterReference)
+                {
+                    IParameterSymbol parameter = parameterReference.Parameter;
+
+                    if (seenAssignmentPerParameter.ContainsKey(parameter))
+                    {
+                        seenAssignmentPerParameter[parameter] = true;
+                    }
+                }
             }
         }
     }
