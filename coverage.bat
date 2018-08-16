63d4e05e542256c7a7040a388744ef7c46c3c36f
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2018-08-16 15:21:24 +0200

    Package updates

--- coverage.bat
+++ coverage.bat
@@ -5,7 +5,7 @@ md coverage
 set configuration=Debug
 set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe"
 set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe"
-set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.3.1\tools\net452\xunit.console.x86.exe"
+set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.4.0\tools\net452\xunit.console.x86.exe"
 set target=".\src\CSharpGuidelinesAnalyzer\CSharpGuidelinesAnalyzer.Test\bin\%configuration%\net46\CSharpGuidelinesAnalyzer.Test.dll -noshadow"
 set filter="+[CSharpGuidelinesAnalyzer*]*  -[CSharpGuidelinesAnalyzer.Test*]*"
 set coveragefile=".\coverage\CodeCoverage.xml"
