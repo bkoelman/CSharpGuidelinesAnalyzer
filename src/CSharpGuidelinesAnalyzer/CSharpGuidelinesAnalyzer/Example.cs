e1b052629c3208609fb1045a129e97a02cff23a2
bart-degreed <bart@degreed.com>
2021-02-27 16:57:30 +0100

    Added code

---
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Example.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+
+namespace CSharpGuidelinesAnalyzer
+{
+    public sealed class Example
+    {
+        [NotNull]
+        public string GetText([NotNull] string text)
+        {
+            return text;
+        }
+    }
+}
