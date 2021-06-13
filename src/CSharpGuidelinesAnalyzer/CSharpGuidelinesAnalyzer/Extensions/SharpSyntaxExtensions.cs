989c14757ba06d4b722e169f2918fea11bc9ec1a
bkoelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2021-06-13 18:17:40 +0200

    Adding analyzer for file member order...

---
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/Extensions/SharpSyntaxExtensions.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CSharpGuidelinesAnalyzer.Extensions
+{
+    internal static class SharpSyntaxExtensions
+    {
+        [CanBeNull]
+        public static BaseTypeDeclarationSyntax TryGetContainingTypeDeclaration([NotNull] this SyntaxNode syntax)
+        {
+            Guard.NotNull(syntax, nameof(syntax));
+
+            SyntaxNode parent = syntax.Parent;
+
+            while (parent != null)
+            {
+                if (parent is BaseTypeDeclarationSyntax typeSyntax)
+                {
+                    return typeSyntax;
+                }
+
+                parent = parent.Parent;
+            }
+
+            return null;
+        }
+    }
+}
