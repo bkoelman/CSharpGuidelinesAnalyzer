6432e68ae4e39659dc9e2bd81816b62bd205e386
Bart Koelman <bartkoelmanNOSPAM@NOSPAMgmail.com>
2022-02-02 12:09:05 +0100

    Adapted help links to new folder structure introduced in CSharpGuidelines v5.5

--- src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/AnalyzerCategory.cs
+++ src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/AnalyzerCategory.cs
@@ -1,45 +1,42 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 
 namespace CSharpGuidelinesAnalyzer
 {
     internal sealed class AnalyzerCategory
     {
-        private const string CommitHash = "7a66f7468da6ce1477753a02e416e04bc9a44e45";
+        private const string CommitHash = "4ad2ebe71296a1d6e70d2f22f7b997edcd9e257d";
 
         [NotNull]
-        public static readonly AnalyzerCategory ClassDesign = new AnalyzerCategory("Class Design", "1000_ClassDesignGuidelines.md");
+        public static readonly AnalyzerCategory ClassDesign = new AnalyzerCategory("Class Design");
 
         [NotNull]
-        public static readonly AnalyzerCategory MemberDesign = new AnalyzerCategory("Member Design", "1100_MemberDesignGuidelines.md");
+        public static readonly AnalyzerCategory MemberDesign = new AnalyzerCategory("Member Design");
 
         [NotNull]
-        public static readonly AnalyzerCategory MiscellaneousDesign = new AnalyzerCategory("Miscellaneous Design", "1200_MiscellaneousDesignGuidelines.md");
+        public static readonly AnalyzerCategory MiscellaneousDesign = new AnalyzerCategory("Miscellaneous Design");
 
         [NotNull]
-        public static readonly AnalyzerCategory Maintainability = new AnalyzerCategory("Maintainability", "1500_MaintainabilityGuidelines.md");
+        public static readonly AnalyzerCategory Maintainability = new AnalyzerCategory("Maintainability");
 
         [NotNull]
-        public static readonly AnalyzerCategory Naming = new AnalyzerCategory("Naming", "1700_NamingGuidelines.md");
+        public static readonly AnalyzerCategory Naming = new AnalyzerCategory("Naming");
 
         [NotNull]
-        public static readonly AnalyzerCategory Framework = new AnalyzerCategory("Framework", "2200_FrameworkGuidelines.md");
+        public static readonly AnalyzerCategory Framework = new AnalyzerCategory("Framework");
 
         [NotNull]
-        public static readonly AnalyzerCategory Documentation = new AnalyzerCategory("Documentation", "2300_DocumentationGuidelines.md");
+        public static readonly AnalyzerCategory Documentation = new AnalyzerCategory("Documentation");
 
         [NotNull]
-        public static readonly AnalyzerCategory Layout = new AnalyzerCategory("Layout", "2400_LayoutGuidelines.md");
-
-        [NotNull]
-        private readonly string helpCategoryUri;
+        public static readonly AnalyzerCategory Layout = new AnalyzerCategory("Layout");
 
         [NotNull]
         public string DisplayName { get; }
 
-        private AnalyzerCategory([NotNull] string displayName, [NotNull] string documentName)
+        private AnalyzerCategory([NotNull] string displayName)
         {
             DisplayName = displayName;
-            helpCategoryUri = $"https://github.com/dennisdoomen/CSharpGuidelines/blob/{CommitHash}/_pages/{documentName}";
         }
 
         [NotNull]
@@ -47,7 +44,14 @@ namespace CSharpGuidelinesAnalyzer
         {
             Guard.NotNullNorWhiteSpace(ruleId, nameof(ruleId));
 
-            return helpCategoryUri + "#" + ruleId.ToLowerInvariant();
+            if (!ruleId.StartsWith("AV", StringComparison.Ordinal) || ruleId.Length != 6)
+            {
+                throw new InvalidOperationException($"Rule '{ruleId}' does not match the format AVnnnn.");
+            }
+
+            string ruleNumber = ruleId.Substring(2);
+            return $"https://github.com/dennisdoomen/CSharpGuidelines/blob/{CommitHash}/_rules/{ruleNumber}.md";
+
         }
     }
 }
