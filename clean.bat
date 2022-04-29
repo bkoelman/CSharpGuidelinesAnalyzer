330b35c3067dc5b16571c3577256955550371fce
Bart Koelman <10324372+bkoelman@users.noreply.github.com>
2022-04-29 19:37:38 +0200

    Replace OpenCover with Coverlet

--- clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /R %%x in (bin) do if exist "%%x" rmdir /s /q "%%x" 1>NUL 2>NUL
-for /R %%x in (obj) do if exist "%%x" rmdir /s /q "%%x" 1>NUL 2>NUL
-
-rmdir /s /q src\.vs 1>NUL 2>NUL
-rmdir /s /q src\packages 1>NUL 2>NUL
-rmdir /s /q coverage 1>NUL 2>NUL
-
-del /f /s /q *.user 1>NUL 2>NUL
--- coverage.bat
+++ coverage.bat
@@ -1,14 +1,6 @@
 @echo off
 rd /s /q coverage 2>nul
-md coverage
+del /s /q coverage.cobertura.xml 2>nul
 
-set configuration=Debug
-set opencover="%USERPROFILE%\.nuget\packages\OpenCover\4.7.1221\tools\OpenCover.Console.exe"
-set reportgenerator="%USERPROFILE%\.nuget\packages\ReportGenerator\5.1.5\tools\net47\ReportGenerator.exe"
-set testrunner="%USERPROFILE%\.nuget\packages\xunit.runner.console\2.4.1\tools\net472\xunit.console.x86.exe"
-set target=".\src\CSharpGuidelinesAnalyzer\CSharpGuidelinesAnalyzer.Test\bin\%configuration%\net48\CSharpGuidelinesAnalyzer.Test.dll -noshadow"
-set filter="+[CSharpGuidelinesAnalyzer*]*  -[CSharpGuidelinesAnalyzer.Test*]*"
-set coveragefile=".\coverage\CodeCoverage.xml"
-
-%opencover% -register:user -target:%testrunner% -targetargs:%target% -filter:%filter% -hideskipped:All -output:%coveragefile%
-%reportgenerator% -targetdir:.\coverage -reports:%coveragefile%
+dotnet test src --configuration Release --no-build --collect:"XPlat Code Coverage"
+dotnet reportgenerator -reports:**\coverage.cobertura.xml -targetdir:coverage
