bkoelman/CSharpGuidelinesAnalyzer

commit 8aab9907e5e55b55a1e145c77b7221fb31247954

Package updates and primary constructors fix (#156)
* Update Resharper
* Package updates
* Run tests against .NET 8, update to latest C# version
* Resharper: use file-scoped namespaces
* Resharper: use target-typed new
* Resharper: use collection expression
* Resharper: use raw string
* Resharper: use pattern syntax
* Resharper: use primary constructor
* Resharper: make struct readonly
* Resharper: remove redundant discard
* Resharper: remove redundant casts
* Resharper: reformat solution
* Fixed AV1500: Do not report on captured variables from primary constructor initializer

src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/DiagnosticCollector.cs
@@ -1,57 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 
-namespace CSharpGuidelinesAnalyzer
+namespace CSharpGuidelinesAnalyzer;
+
+internal sealed class DiagnosticCollector : IDisposable
 {
-    internal sealed class DiagnosticCollector : IDisposable
+    [NotNull]
+    private readonly Action<Diagnostic> reportDiagnostic;
+
+    [CanBeNull]
+    [ItemNotNull]
+    private List<Diagnostic> diagnostics;
+
+    [NotNull]
+    [ItemNotNull]
+    public ICollection<Diagnostic> Diagnostics
     {
-        [NotNull]
-        private readonly Action<Diagnostic> reportDiagnostic;
-
-        [CanBeNull]
-        [ItemNotNull]
-        private List<Diagnostic> diagnostics;
-
-        [NotNull]
-        [ItemNotNull]
-        public ICollection<Diagnostic> Diagnostics
+        get
         {
-            get
+            if (diagnostics == null)
             {
-                if (diagnostics == null)
-                {
-                    return ImmutableArray<Diagnostic>.Empty;
-                }
-
-                return diagnostics;
+                return [];
             }
+
+            return diagnostics;
         }
+    }
 
-        public DiagnosticCollector([NotNull] Action<Diagnostic> reportDiagnostic)
+    public DiagnosticCollector([NotNull] Action<Diagnostic> reportDiagnostic)
+    {
+        Guard.NotNull(reportDiagnostic, nameof(reportDiagnostic));
+
+        diagnostics = null;
+        this.reportDiagnostic = reportDiagnostic;
+    }
+
+    public void Add([NotNull] Diagnostic diagnostic)
+    {
+        Guard.NotNull(diagnostic, nameof(diagnostic));
+
+        diagnostics ??= [];
+        diagnostics.Add(diagnostic);
+    }
+
+    public void Dispose()
+    {
+        foreach (Diagnostic diagnostic in Diagnostics)
         {
-            Guard.NotNull(reportDiagnostic, nameof(reportDiagnostic));
-
-            diagnostics = null;
-            this.reportDiagnostic = reportDiagnostic;
-        }
-
-        public void Add([NotNull] Diagnostic diagnostic)
-        {
-            Guard.NotNull(diagnostic, nameof(diagnostic));
-
-            diagnostics ??= new List<Diagnostic>();
-            diagnostics.Add(diagnostic);
-        }
-
-        public void Dispose()
-        {
-            foreach (Diagnostic diagnostic in Diagnostics)
-            {
-                reportDiagnostic(diagnostic);
-            }
+            reportDiagnostic(diagnostic);
         }
     }
 }

commit 0c3e9cdce39283ed6f21fcf7d84066c49d5b4eb9

Fixed: do not allocate collection (regression from #156)

src/CSharpGuidelinesAnalyzer/CSharpGuidelinesAnalyzer/DiagnosticCollector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 
@@ -22,7 +23,7 @@ internal sealed class DiagnosticCollector : IDisposable
         {
             if (diagnostics == null)
             {
-                return [];
+                return ImmutableArray<Diagnostic>.Empty;
             }
 
             return diagnostics;